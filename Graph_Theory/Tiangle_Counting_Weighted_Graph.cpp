fdf02b79e6f2831ca17ec3514808e303ae589643
ishan dutta <ishandutta2007@gmail.com>
2022-06-18 14:33:14 +0530

    formatting

---
+++ Graph_Theory/Tiangle_Counting_Weighted_Graph.cpp
@@ -0,0 +1,21 @@
+using ll = long long;
+ll count_triangle(auto edges) {
+  ll ret = 0;
+  std::vector<int> mark(n + 1, -1);
+  for (int a = 1; a <= n; ++a) {
+    for (auto &&e : edges[a])
+      mark[e.first] = e.second;
+    for (auto &&e : edges[a]) {
+      int b = e.first, w1 = e.second;
+      for (auto &&ee : edges[b]) {
+        int c = ee.first, w2 = ee.second;
+        if (mark[c] != -1) {
+          ret += w1 * w2 * mark[c];
+        }
+      }
+    }
+    for (auto &&e : edges[a])
+      mark[e.first] = -1;
+  }
+  return ret;
+}
