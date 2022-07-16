7a6b68003cd95726d5ce12700e1fc39a424b9ba1
Ishan <ishandutta2007@gmail.com>
2022-07-16 21:45:01 +0530

    Graph_Theory/shortest_path_k_edges.cpp

---
+++ Geometry/shoelace.py
@@ -0,0 +1,34 @@
+#The Shoelace Algorithm - www.101computing.net/the-shoelace-algorithm
+
+def polygonArea(vertices):
+  #A function to apply the Shoelace algorithm
+  numberOfVertices = len(vertices)
+  sum1 = 0
+  sum2 = 0
+  
+  for i in range(0,numberOfVertices-1):
+    sum1 = sum1 + vertices[i][0] *  vertices[i+1][1]
+    sum2 = sum2 + vertices[i][1] *  vertices[i+1][0]
+  
+  #Add xn.y1
+  sum1 = sum1 + vertices[numberOfVertices-1][0]*vertices[0][1]   
+  #Add x1.yn
+  sum2 = sum2 + vertices[0][0]*vertices[numberOfVertices-1][1]   
+  
+  area = abs(sum1 - sum2) / 2
+  return area
+  
+#Vertices (x,y) Coordinates  
+A = [2,7]
+B = [10,1]
+C = [8,6]
+D = [11,7]
+E = [7,10]
+#Define a polygon as being a list of vertices, (on anticlockwise order)
+polygon = [A,B,C,D,E]  
+
+area = polygonArea(polygon)
+print("Polygon Vertices:")
+print(polygon)
+print("")
+print("Area = " + str(area) + "cm2")
--- Graph_Theory/centroid_decomposition.cpp
+++ Graph_Theory/centroid_decomposition.cpp
@@ -1,167 +1,154 @@
-// C++ program for centroid decomposition of Tree
 #include <bits/stdc++.h>
 using namespace std;
 
-#define MAXN 1025
+const int N = 1e5 + 9;
 
-vector<int> tree[MAXN];
-vector<int> centroidTree[MAXN];
-bool centroidMarked[MAXN];
-
-/* method to add edge between to nodes of the undirected tree */
-void addEdge(int u, int v)
-{
-	tree[u].push_back(v);
-	tree[v].push_back(u);
+const double PI = acos(-1);
+struct base {
+  double a, b;
+  base(double a = 0, double b = 0) : a(a), b(b) {}
+  const base operator+(const base &c) const { return base(a + c.a, b + c.b); }
+  const base operator-(const base &c) const { return base(a - c.a, b - c.b); }
+  const base operator*(const base &c) const {
+    return base(a * c.a - b * c.b, a * c.b + b * c.a);
+  }
+};
+void fft(vector<base> &p, bool inv = 0) {
+  int n = p.size(), i = 0;
+  for (int j = 1; j < n - 1; ++j) {
+    for (int k = n >> 1; k > (i ^= k); k >>= 1)
+      ;
+    if (j < i)
+      swap(p[i], p[j]);
+  }
+  for (int l = 1, m; (m = l << 1) <= n; l <<= 1) {
+    double ang = 2 * PI / m;
+    base wn = base(cos(ang), (inv ? 1. : -1.) * sin(ang)), w;
+    for (int i = 0, j, k; i < n; i += m) {
+      for (w = base(1, 0), j = i, k = i + l; j < k; ++j, w = w * wn) {
+        base t = w * p[j + l];
+        p[j + l] = p[j] - t;
+        p[j] = p[j] + t;
+      }
+    }
+  }
+  if (inv)
+    for (int i = 0; i < n; ++i)
+      p[i].a /= n, p[i].b /= n;
+}
+vector<long long> multiply(vector<int> &a, vector<int> &b) {
+  int n = a.size(), m = b.size(), t = n + m - 1, sz = 1;
+  while (sz < t)
+    sz <<= 1;
+  vector<base> x(sz), y(sz), z(sz);
+  for (int i = 0; i < sz; ++i) {
+    x[i] = i < (int)a.size() ? base(a[i], 0) : base(0, 0);
+    y[i] = i < (int)b.size() ? base(b[i], 0) : base(0, 0);
+  }
+  fft(x), fft(y);
+  for (int i = 0; i < sz; ++i)
+    z[i] = x[i] * y[i];
+  fft(z, 1);
+  vector<long long> ret(sz);
+  for (int i = 0; i < sz; ++i)
+    ret[i] = z[i].a + 0.5;
+  while ((int)ret.size() > 1 && ret.back() == 0)
+    ret.pop_back();
+  return ret;
 }
 
-/* method to setup subtree sizes and nodes in current tree */
-void DFS(int src, bool visited[], int subtree_size[], int* n)
-{
-	/* mark node visited */
-	visited[src] = true;
-
-	/* increase count of nodes visited */
-	*n += 1;
-
-	/* initialize subtree size for current node*/
-	subtree_size[src] = 1;
-
-	vector<int>::iterator it;
-
-	/* recur on non-visited and non-centroid neighbours */
-	for (it = tree[src].begin(); it!=tree[src].end(); it++)
-		if (!visited[*it] && !centroidMarked[*it])
-		{
-			DFS(*it, visited, subtree_size, n);
-			subtree_size[src]+=subtree_size[*it];
-		}
+vector<int> g[N];
+int sz[N];
+int tot, done[N], cenpar[N];
+void calc_sz(int u, int p) {
+  tot++;
+  sz[u] = 1;
+  for (auto v : g[u]) {
+    if (v == p || done[v])
+      continue;
+    calc_sz(v, u);
+    sz[u] += sz[v];
+  }
 }
-
-int getCentroid(int src, bool visited[], int subtree_size[], int n)
-{
-	/* assume the current node to be centroid */
-	bool is_centroid = true;
-
-	/* mark it as visited */
-	visited[src] = true;
-
-	/* track heaviest child of node, to use in case node is
-	not centroid */
-	int heaviest_child = 0;
-
-	vector<int>::iterator it;
-
-	/* iterate over all adjacent nodes which are children
-	(not visited) and not marked as centroid to some
-	subtree */
-	for (it = tree[src].begin(); it!=tree[src].end(); it++)
-		if (!visited[*it] && !centroidMarked[*it])
-		{
-			/* If any adjacent node has more than n/2 nodes,
-			* current node cannot be centroid */
-			if (subtree_size[*it]>n/2)
-				is_centroid=false;
-
-			/* update heaviest child */
-			if (heaviest_child==0 ||
-				subtree_size[*it]>subtree_size[heaviest_child])
-				heaviest_child = *it;
-		}
-
-	/* if current node is a centroid */
-	if (is_centroid && n-subtree_size[src]<=n/2)
-		return src;
-
-	/* else recur on heaviest child */
-	return getCentroid(heaviest_child, visited, subtree_size, n);
+int find_cen(int u, int p) {
+  for (auto v : g[u]) {
+    if (v == p || done[v])
+      continue;
+    else if (sz[v] > tot / 2)
+      return find_cen(v, u);
+  }
+  return u;
 }
-
-/* function to get the centroid of tree rooted at src.
-* tree may be the original one or may belong to the forest */
-int getCentroid(int src)
-{
-	bool visited[MAXN];
-
-	int subtree_size[MAXN];
-
-	/* initialize auxiliary arrays */
-	memset(visited, false, sizeof visited);
-	memset(subtree_size, 0, sizeof subtree_size);
-
-	/* variable to hold number of nodes in the current tree */
-	int n = 0;
-
-	/* DFS to set up subtree sizes and nodes in current tree */
-	DFS(src, visited, subtree_size, &n);
-
-	for (int i=1; i<MAXN; i++)
-		visited[i] = false;
-
-	int centroid = getCentroid(src, visited, subtree_size, n);
-
-	centroidMarked[centroid]=true;
-
-	return centroid;
+vector<int> vec;
+void dfs(int u, int pre, int len) {
+  vec.push_back(len);
+  for (auto v : g[u]) {
+    if (v == pre || done[v])
+      continue;
+    dfs(v, u, len + 1);
+  }
 }
-
-/* function to generate centroid tree of tree rooted at src */
-int decomposeTree(int root)
-{
-	//printf("decomposeTree(%d)\n", root);
-
-	/* get centroid for current tree */
-	int cend_tree = getCentroid(root);
-
-	printf("%d ", cend_tree);
-
-	vector<int>::iterator it;
-
-	/* for every node adjacent to the found centroid
-	* and not already marked as centroid */
-	for (it=tree[cend_tree].begin(); it!=tree[cend_tree].end(); it++)
-	{
-		if (!centroidMarked[*it])
-		{
-			/* decompose subtree rooted at adjacent node */
-			int cend_subtree = decomposeTree(*it);
-
-			/* add edge between tree centroid and centroid of subtree */
-			centroidTree[cend_tree].push_back(cend_subtree);
-			centroidTree[cend_subtree].push_back(cend_tree);
-		}
-	}
-
-	/* return centroid of tree */
-	return cend_tree;
+long long ans[N]; // number of paths of length i = ans[i]
+void solve(int u, int pre, int len, int add) {
+  vec.clear();
+  dfs(u, pre, len);
+  int mx = 0;
+  for (auto x : vec)
+    mx = max(mx, x);
+  vector<int> a(mx + 1, 0);
+  for (auto x : vec)
+    a[x]++;
+  vector<long long> res = multiply(a, a);
+  for (int i = 0; i < res.size(); i++)
+    ans[i] += res[i] * add;
 }
-
-// driver function
-int main()
-{
-	/* number of nodes in the tree */
-	int n = 16;
-
-	/* arguments in order: node u, node v
-	* sequencing starts from 1 */
-	addEdge(1, 4);
-	addEdge(2, 4);
-	addEdge(3, 4);
-	addEdge(4, 5);
-	addEdge(5, 6);
-	addEdge(6, 7);
-	addEdge(7, 8);
-	addEdge(7, 9);
-	addEdge(6, 10);
-	addEdge(10, 11);
-	addEdge(11, 12);
-	addEdge(11, 13);
-	addEdge(12, 14);
-	addEdge(13, 15);
-	addEdge(13, 16);
-
-	/* generates centroid tree */
-	decomposeTree(1);
-
-	return 0;
+void decompose(int u, int pre) {
+  tot = 0;
+  calc_sz(u, pre);
+  int cen = find_cen(u, pre);
+  cenpar[cen] = pre;
+  done[cen] = 1;
+  solve(cen, pre, 0, 1);
+  for (auto v : g[cen]) {
+    if (v == pre || done[v])
+      continue;
+    solve(v, cen, 1, -1);
+  }
+  for (auto v : g[cen]) {
+    if (v == pre || done[v])
+      continue;
+    decompose(v, cen);
+  }
 }
+int main() {
+  freopen("awesome.in", "r", stdin);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n, l, r;
+    cin >> n >> l >> r;
+    l = n - 1 - l;
+    r = n - 1 - r;
+    swap(l, r);
+    for (int i = 1; i < n; i++) {
+      int u, v;
+      cin >> u >> v;
+      g[u].push_back(v);
+      g[v].push_back(u);
+    }
+    decompose(1, 0);
+    ans[0] = 0;
+    long long res = 0;
+    for (int i = l; i <= r; i++)
+      res += ans[i] / 2;
+    cout << res << '\n';
+    for (int i = 1; i <= n; i++)
+      g[i].clear(), done[i] = 0;
+    for (int i = 0; i < n; i++)
+      ans[i] = 0;
+  }
+  return 0;
+}
+// https://codeforces.com/gym/101991/problem/A
