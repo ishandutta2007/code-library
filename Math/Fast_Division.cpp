92bac0d52ff1929d825a9143bcf0f2d04fbd5622
Ishan <ishandutta2007@gmail.com>
2022-09-17 21:21:55 +0530

    rename

---
+++ Math/Fast_Division.cpp
@@ -0,0 +1,5 @@
+#define DIVIDE(n, d) ((FastInt)((double)(n) / d))
+// or
+auto fast_div = [](const ll &a, const int &b) -> ll {
+  return double(a) / b + 1e-9;
+};
--- Math/fast_div.cpp
+++
@@ -1,3 +0,0 @@
-#define DIVIDE(n, d) ((FastInt)((double)(n)/d))
-// or
-auto fast_div=[](const ll& a,const int& b)->ll {return double(a)/b+1e-9;};
