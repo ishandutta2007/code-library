54cb2420a13bae1176b0e9591e5a73bca09006ee
Ishan <ishandutta2007@gmail.com>
2022-09-21 17:14:34 +0530

    Graph_Theory/Maximal_Matching.cpp

--- Number_Theory/ModInverse_one.cpp
+++ Number_Theory/ModInverse_one.cpp
@@ -1,2 +1,13 @@
 
 ll mod_inv(x, mod) { return bigpow(x, mod - 2, mod); }
+
+    mint inv() const {
+        if (prime) {
+            assert(_v);
+            return pow(umod() - 2);
+        } else {
+            auto eg = internal::inv_gcd(_v, m);
+            assert(eg.first == 1);
+            return eg.second;
+        }
+    }
