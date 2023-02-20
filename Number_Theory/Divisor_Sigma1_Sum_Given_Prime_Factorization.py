4db3c9a9e9dc81a376ba2dc61e4b4d41f8458002
ishan dutta <ishandutta2007@gmail.com>
2023-02-20 13:43:24 +0530

    Update and rename Divisor_Sigma0_Sum_Given_Prime_Factorization.py to Divisor_Sigma1_Sum_Given_Prime_Factorization.py

--- Number_Theory/Divisor_Sigma1_Sum_Given_Prime_Factorization.py
+++ Number_Theory/Divisor_Sigma1_Sum_Given_Prime_Factorization.py
@@ -13,12 +13,3 @@ def sum_of_divisors(prime_factorization):
     return _sum
 
 
-def generate_divisors(prime_factorization):
-    divisors = [1]
-    for prime in prime_factorization.keys():
-        new_divisors = []
-        for divisor in divisors:
-            for i in range(1, prime_factorization[prime] + 1):
-                new_divisors.append((divisor * prime ** i)%MOD)
-        divisors.extend(new_divisors)
-    return divisors
